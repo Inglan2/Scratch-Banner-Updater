1b101077abde6608386886769899fa02938a004f
ingo wolf <0358381@schoolsnet.act.edu.au>
2024-03-22 01:13:54 +0000

    add testing code

--- generate.py
+++ generate.py
@@ -1,1 +1,13 @@
+import os
+os.system("pip install -U scratchattach")
+import scratchattach as scratch3
 
+user = scratch3.get_user("Inglan1")
+
+print(user.join_date)
+print(user.about_me)
+print(user.wiwo)
+print(user.country)
+print(user.icon_url)
+print(user.id)
+print(user.scratchteam)
